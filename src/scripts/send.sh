48865ec6ad2b1c01c99b2c3036117a610b3b49e6
ichi-01 <ichikawa@rhems-japan.co.jp>
2021-12-22 19:11:57 +0900

    fix

--- src/scripts/send.sh
+++ src/scripts/send.sh
@@ -1,2 +1,2 @@
-curl -X POST -H "X-ChatWorkToken: ${TOKEN}" \
+curl -X POST -H "X-ChatWorkToken: ${CHATWORK_TOKEN}" \
     -d "body=${BODY}&self_unread=${IS_UNREAD}" "https://api.chatwork.com/v2/rooms/${ROOMID}/messages"
