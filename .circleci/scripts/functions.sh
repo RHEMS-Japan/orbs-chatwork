f2a04f7e4e1c380ab6577e5f0cf95aeddb31a82c
ichi-01 <ichikawa@rhems-japan.co.jp>
2021-12-22 18:21:32 +0900

    fix

--- .circleci/scripts/functions.sh
+++ .circleci/scripts/functions.sh
@@ -7,10 +7,11 @@ function create_message() {
 
     #body
     MESSAGE=`cat << EOS
-    Project : orbs-chatwork
-    Repo : alpha
-    Time : 2021-12-22 18:00:00
-    EOS`
+Project : orbs-chatwork
+Repo : alpha
+Time : 2021-12-22 18:00:00
+EOS`
+    
     TITLE="Orb作成中"
     SUB_TITLE="試験投稿です"
     echo "export BODY="[info][title]${TITLE}[/title]${SUB_TITLE}[preview id=${ALERT_RESOLVED} ht=60][hr]${MESSAGE}[/info]" >> $BASH_ENV
