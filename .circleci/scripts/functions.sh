d5a6f2069b37e986ca6db1b0b102e6f7a8e3dda4
ichi-01 <ichikawa@rhems-japan.co.jp>
2021-12-22 18:53:17 +0900

    fix

--- .circleci/scripts/functions.sh
+++ .circleci/scripts/functions.sh
@@ -1,10 +1,3 @@
-function here_doc() {
-cat <<EOF
-Project : orbs-chatwork
-Repo : alpha
-Time : 2021-12-22 18:00:00
-EOF
-}
 function create_message() {
 # images
 ALERT_FIRING="864720511"
@@ -12,7 +5,12 @@ ALERT_RESOLVED="864720669"
 WARNING="864724297"
 
 #body
-MESSAGE=$( here_doc )
+MESSAGE=$(cat <<EOF
+Project : orbs-chatwork
+Repo : alpha
+Time : 2021-12-22 18:00:00
+EOF
+)
 
 TITLE="Orb作成中"
 SUB_TITLE="試験投稿です"
