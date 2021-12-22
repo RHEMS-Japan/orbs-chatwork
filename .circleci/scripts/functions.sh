dd79c2afb6b90118a709c3321f27547201c13cfc
ichi-01 <ichikawa@rhems-japan.co.jp>
2021-12-22 19:09:05 +0900

    fix

--- .circleci/scripts/functions.sh
+++ .circleci/scripts/functions.sh
@@ -5,14 +5,14 @@ ALERT_RESOLVED="864720669"
 WARNING="864724297"
 
 #body
-MESSAGE=$(cat <<EOF
+MESSAGE=`cat <<EOF
 Project : orbs-chatwork
 Repo : alpha
 Time : 2021-12-22 18:00:00
 EOF
-)
+`
 
 TITLE="Orb作成中"
 SUB_TITLE="試験投稿です"
-echo "export BODY="[info][title]${TITLE}[/title]${SUB_TITLE}[preview id=${ALERT_RESOLVED} ht=60][hr]${MESSAGE}[/info]" >> $BASH_ENV
+echo "export BODY='[info][title]${TITLE}[/title]${SUB_TITLE}[preview id=${ALERT_RESOLVED} ht=60][hr]${MESSAGE}[/info]'" >> $BASH_ENV
 }
