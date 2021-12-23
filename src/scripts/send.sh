8b9ca771cc764b469fb68e5b60a48f2f10dab4fb
ichi-01 <ichikawa@rhems-japan.co.jp>
2021-12-23 12:34:31 +0900

    [semver:skip] Add a process to terminate Ci when the curl command fails.

--- src/scripts/send.sh
+++ src/scripts/send.sh
@@ -1,7 +1,4 @@
-echo "${CHATWORK_TOKEN}"
-echo "${BODY}"
-echo "${IS_UNREAD}"
+RESULT=`curl -s -o /dev/null -w '%{http_code}\n' -X POST -H "X-ChatWorkToken: ${CHATWORK_TOKEN}" \
+    -d "body=${BODY}&self_unread=1" "https://api.chatwork.com/${VER}/rooms/${ROOMID}/messages"`
 
-
-curl -X POST -H "X-ChatWorkToken: ${CHATWORK_TOKEN}" \
-    -d "body=${BODY}&self_unread=${IS_UNREAD}" "https://api.chatwork.com/${VER}/rooms/${ROOMID}/messages"
+echo [ 200 = $RESULT ] || exit 1
