2325ef3b34deb1b0c707421338bda11b23da7a99
ichi-01 <ichikawa@rhems-japan.co.jp>
2021-12-23 11:47:44 +0900

    test

--- src/scripts/send.sh
+++ src/scripts/send.sh
@@ -1,2 +1,8 @@
-curl -X POST -H "X-ChatWorkToken: ${CHATWORK_TOKEN}" \
-    -d "body=${BODY}&self_unread=${IS_UNREAD}" "https://api.chatwork.com/v2/rooms/${ROOMID}/messages"
+TOKEN=`eval echo ${TOKEN}`
+echo ${TOKEN}
+echo ${BODY}
+echo ${IS_UNREAD}
+
+
+curl -X POST -H "X-ChatWorkToken: ${TOKEN}" \
+    -d "body=${BODY}&self_unread=${IS_UNREAD}" "https://api.chatwork.com/${VER}/rooms/${ROOMID}/messages"
