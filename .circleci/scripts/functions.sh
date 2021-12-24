bea253fec60fa3097788bacfd44bf57ffcee787f
ichi-01 <ichikawa@rhems-japan.co.jp>
2021-12-25 06:29:38 +0900

    test workflow url

--- .circleci/scripts/functions.sh
+++ .circleci/scripts/functions.sh
@@ -7,12 +7,12 @@ ERROR="866484114"
 MESSAGE=`cat <<EOF
 Project : orbs-chatwork
 Repo : alpha
-Time : "$(date "+%Y/%m/%d %H:%M:%S")"
+Time : $(date "+%Y/%m/%d %H:%M:%S")
+https://circleci.com/workflow-run/${CIRCLE_WORKFLOW_ID}
 EOF
 `
 BODY="[preview id=${SUCCESS} ht=60][hr]${MESSAGE}"
 
 echo "export BODY='${BODY}'" >> $BASH_ENV
-# echo "export BODY='[preview id=${SUCCESS} ht=60][hr]${MESSAGE}'" >> $BASH_ENV
 echo "${BODY}"
 }
