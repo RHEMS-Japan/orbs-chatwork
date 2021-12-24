dbd111b0fd0867230c150766e206e1ddfd44e55d
ichi-01 <ichikawa@rhems-japan.co.jp>
2021-12-25 06:08:20 +0900

    test

--- .circleci/scripts/functions.sh
+++ .circleci/scripts/functions.sh
@@ -10,8 +10,9 @@ Repo : alpha
 Time : 2021/12/24 14:46:45
 EOF
 `
-# BODY="[preview id=${SUCCESS} ht=60][hr]${MESSAGE}"
-BODY="testtest"
+BODY="[preview id=${SUCCESS} ht=60][hr]${MESSAGE}"
+
+# echo "export BODY=${BODY}" >> $BASH_ENV
+echo "export BODY=$(eval echo '\$BODY')" >> $BASH_ENV
 echo "${BODY}"
-echo "export BODY=${BODY}" >> $BASH_ENV
 }
