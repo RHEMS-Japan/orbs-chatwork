bac83fa87e06700360079e535a8af1d7585e32a8
ichi-01 <ichikawa@rhems-japan.co.jp>
2021-12-24 14:16:24 +0900

    test

--- .circleci/scripts/functions.sh
+++ .circleci/scripts/functions.sh
@@ -2,14 +2,14 @@ function create_message() {
 # images
 SUCCESS="866484042"
 ERROR="866484114"
-
+DATE=$(date)
 #body
-MESSAGE=`cat <<EOF
+MESSAGE=$(cat <<EOF
 Project : orbs-chatwork
 Repo : alpha
-Time : $(date)
+Time : ${DATE}
 EOF
-`
+)
 BODY="[preview id=${SUCCESS} ht=60][hr]${MESSAGE}"
 
 echo "export BODY=${BODY}" >> $BASH_ENV
