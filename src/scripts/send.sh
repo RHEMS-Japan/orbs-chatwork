b4a80c7bf924045164cd92e2a1195bb10b1abd3b
ichi-01 <ichikawa@rhems-japan.co.jp>
2021-12-24 13:11:09 +0900

    [semver:skip] fix send.sh

--- src/scripts/send.sh
+++ src/scripts/send.sh
@@ -1,4 +1,4 @@
 RESULT=$(curl -s -o /dev/null -w '%{http_code}\n' -X POST -H "X-ChatWorkToken: ${CHATWORK_TOKEN}" \
-    -d "body=${BODY}&self_unread=1" "https://api.chatwork.com/${VER}/rooms/${ROOMID}/messages")
+    -d "body=${BODY}&self_unread=${IS_UNREAD}" "https://api.chatwork.com/${VER}/rooms/${ROOMID}/messages")
 
-echo [ 200 = "$RESULT" ] || exit 1
+[ "200" = "${RESULT}" ] || exit 1
