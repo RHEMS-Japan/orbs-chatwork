f8e5654881b95dba8e34083cd2a557c77c9b807f
ichi-01 <ichikawa@rhems-japan.co.jp>
2021-12-25 06:19:53 +0900

    test

--- .circleci/scripts/functions.sh
+++ .circleci/scripts/functions.sh
@@ -7,11 +7,12 @@ ERROR="866484114"
 MESSAGE=`cat <<EOF
 Project : orbs-chatwork
 Repo : alpha
+Time : "$(date "+%Y/%m/%d %H:%M:%S")"
 EOF
 `
-# BODY="[preview id=${SUCCESS} ht=60][hr]${MESSAGE}"
+BODY="[preview id=${SUCCESS} ht=60][hr]${MESSAGE}"
 
-# echo "export BODY=${BODY}" >> $BASH_ENV
-echo "export BODY='[preview id=${SUCCESS} ht=60][hr]${MESSAGE}'" >> $BASH_ENV
+echo "export BODY='${BODY}'" >> $BASH_ENV
+# echo "export BODY='[preview id=${SUCCESS} ht=60][hr]${MESSAGE}'" >> $BASH_ENV
 echo "${BODY}"
 }
