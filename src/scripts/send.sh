761b080bb8108d3e740b8015cd5190685237149c
ichi-01 <ichikawa@rhems-japan.co.jp>
2022-01-07 14:42:49 +0900

    [semver:skip] test

--- src/scripts/send.sh
+++ src/scripts/send.sh
@@ -1,11 +1,9 @@
 # shellcheck disable=SC2155
-echo -e "${BODY}"
-# _body=$(eval echo -e "${BODY}")
-# echo "$_body"
+echo -e "${_body}"
 echo -e "---"
 
 RESULT=$(curl -s -o /dev/null -w '%{http_code}\n' -X POST -H "X-ChatWorkToken: ${CHATWORK_TOKEN}" \
-    -d "body=${BODY}&self_unread=${IS_UNREAD}" "https://api.chatwork.com/${VER}/rooms/${ROOMID}/messages")
+    -d "body=${_body}&self_unread=${IS_UNREAD}" "https://api.chatwork.com/${VER}/rooms/${ROOMID}/messages")
 
 echo "http_code=${RESULT}"
 [ "200" = "${RESULT}" ] || exit 1
