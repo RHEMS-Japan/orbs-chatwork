8933a948f6b881711a398a65baf02db6b440125a
ichi-01 <ichikawa@rhems-japan.co.jp>
2022-01-07 14:23:33 +0900

    [semver:skip] test

--- src/scripts/send.sh
+++ src/scripts/send.sh
@@ -5,7 +5,7 @@ echo -e "${BODY}"
 echo -e "---"
 
 RESULT=$(curl -s -o /dev/null -w '%{http_code}\n' -X POST -H "X-ChatWorkToken: ${CHATWORK_TOKEN}" \
-    -d "body=${_body}&self_unread=${IS_UNREAD}" "https://api.chatwork.com/${VER}/rooms/${ROOMID}/messages")
+    -d "body=${BODY}&self_unread=${IS_UNREAD}" "https://api.chatwork.com/${VER}/rooms/${ROOMID}/messages")
 
 echo "http_code=${RESULT}"
 [ "200" = "${RESULT}" ] || exit 1
