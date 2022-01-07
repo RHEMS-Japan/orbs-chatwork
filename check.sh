846013c515de02fea5f3e9e76ca4a46de04dfe68
ichi-01 <ichikawa@rhems-japan.co.jp>
2022-01-07 16:26:56 +0900

    test

--- check.sh
+++ check.sh
@@ -8,7 +8,7 @@ circleci orb --skip-update-check validate orb.yml
 echo -e "\n============================"
 echo -e "         Shell check"
 echo -e "============================\n"
-shellcheck -e SC2148 SC2155 src/scripts/*.sh
+shellcheck -e SC2148 src/scripts/*.sh
 echo -e "\n============================"
 echo -e "  CircleCI config Validate"
 echo -e "============================\n"
